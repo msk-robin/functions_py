dbba3117d19ef27608daaf398647a83dce6a9459
robin-msk <robin.musyoki@kibo.school>
2022-12-05 23:48:23 +0300

    added a geometry.py and utilities.py with grade 7th function code

---
+++ geometry.py
@@ -0,0 +1,27 @@
+# from utilities import rectangle_area , triangle_area , circle_area 
+
+# area = 0
+ 
+# shape_choice =("Enter the shape to find its area: \nCircle \nRectangle \nTriangle")
+
+# if shape_choice.lower() =='circle':
+#     radius = input("Please enter the radius of the needed Circle ")
+#     area = circle_area(radius=radius)
+
+# elif shape_choice.lower() == 'rectangle':
+#     length = input("Please enter the lenght of the needed rectangle")
+#     width = input("Please enter the width of the needed rectangle")
+#     area = rectangle_area(length=length, width=width)
+
+# elif shape_choice.lower() == 'triangle':
+#     height = input("Please enter the height of the needed triangle")
+#     base = input("Please enter the base of the needed triangle")
+#     area = circle_area(base=base, height=height)
+
+# else:
+#     print(f"Selected shape is not supported {shape_choice.lower()} - Please choose an item from the list")
+
+# print(f"Calculated area for {shape_choice} is {area}")
+
+
+#This code needs a helper function to work (visit the utilities file)
