666373071e6cd7705adf76cb15c01df5b5076e8f
robin-msk <robin.musyoki@kibo.school>
2023-01-02 11:25:19 +0300

    named functions and variables apropriently

--- geometry.py
+++ geometry.py
@@ -1,27 +1,35 @@
 from utilities import rectangle_area , triangle_area , circle_area 
 
 area = 0
- 
-shape_choice =input("Enter the shape to find its area: \nCircle \nRectangle \nTriangle \n")
 
-if shape_choice.lower() =='circle':
-    radius = float(input("Please enter the radius of the needed Circle: "))
-    area = circle_area(radius)
+# Looping through the circle rectangle Triangle and end
+while True:
+        shape_choice =input("Enter the shape to find its area: \nCircle \nRectangle \nTriangle \n")
 
-elif shape_choice.lower() == 'rectangle':
-    length = float(input("Please enter the lenght of the needed rectanglen: "))
-    width = float(input("Please enter the width of the needed rectangle: "))
-    area = rectangle_area(length,width)
+        if shape_choice.lower() =='circle':
+            radius = float(input("Please enter the radius of the needed Circle: "))
+            area = circle_area(radius) #calling function
+            print(f"Calculated area for {shape_choice} is {area}\n")
+            continue
 
-elif shape_choice.lower() == 'triangle':
-    height = float(input("Please enter the height of the needed triangle: "))
-    base = float(input("Please enter the base of the needed triangle: "))
-    area = triangle_area(base,height)
+        elif shape_choice.lower() == 'rectangle':
+            length = float(input("Please enter the lenght of the needed rectanglen: "))
+            width = float(input("Please enter the width of the needed rectangle: "))
+            area = rectangle_area(length,width)  #calling function
+            print(f"Calculated area for {shape_choice} is {area}\n")
+            continue
 
-else:
-    print(f"Selected shape is not supported {shape_choice} - Please choose an item from the list")
+        elif shape_choice.lower() == 'triangle':
+            height = float(input("Please enter the height of the needed triangle: "))
+            base = float(input("Please enter the base of the needed triangle: "))
+            area = triangle_area(base,height)   #calling function
+            print(f"Calculated area for {shape_choice} is {area}\n")
+            continue
 
-print(f"Calculated area for {shape_choice} is {area}")
+        else: #guiding feedback after unexpected input
+            print(f"Selected shape is not supported {shape_choice} - Please choose an item from the list \n")
+            continue
+
 
 
 #This code needs a helper function to work (visit the utilities file)
--- utilities.py
+++ utilities.py
@@ -1,14 +1,14 @@
 from math import pi
-def circle_area(r):
-    area = float(pi*r*r)
+def circle_area(radius):
+    area = float(pi*radius*radius)
     return round(area,3)
 
-def rectangle_area(l, w):
-    area = float(l * w)
+def rectangle_area(length, width):
+    area = float(length * width)
     return round(area,3)
 
-def triangle_area(b,h):
-    area = float((b*h)/2)
+def triangle_area(base,height):
+    area = float((base*height)/2)
     return round (area,3)
 
 # add a helper function for it to work
